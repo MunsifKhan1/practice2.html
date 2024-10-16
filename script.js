36d6c3d52a449b9d52d104d0acc81de59d5b9f51
MunsifKhan1 <munsifk597@gmail.com>
2024-10-15 23:50:47 -0700

    add a file

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 
+const fruits = ['apple', 'banana', 'cherry'];
+
+const upperCaseFruits = fruits.map((fruit) => {
+    console.log(fruit); // Log the original fruit name
+    return fruit.toUpperCase(); // Return the fruit name in uppercase
+});
